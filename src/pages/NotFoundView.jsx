fc6eaa907c72b67a4b2db79726affb881100c423
Siriusb24 <Siriusb2324@gmail.com>
2021-12-07 19:55:31 +0200

    update

--- src/pages/NotFoundView.jsx
+++
@@ -1,8 +0,0 @@
-export default function NotFoundView({ errorImage, messadge }) {
-  return (
-    <div role="alert">
-      <img src={errorImage} alt="Error 404" />
-      <p>{messadge}</p>
-    </div>
-  );
-}
